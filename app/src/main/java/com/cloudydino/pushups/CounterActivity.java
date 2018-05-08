1d26572e2cc7d173b5b952e3b7c6411837dd5196
Amaan Marfatia <amaanmarfatia@gmail.com>
2018-05-07 22:52:13 -0400

    Setup Android Project

---
+++ app/src/main/java/com/cloudydino/pushups/CounterActivity.java
@@ -0,0 +1,13 @@
+package com.cloudydino.pushups;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+    }
+}
