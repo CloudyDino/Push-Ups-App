40d40020685c5e6f12d4deafa759b4173b872ba6
Amaan Marfatia <amaanmarfatia@gmail.com>
2018-05-08 23:38:25 -0400

    Add menu to counter activity

--- app/src/main/java/com/cloudydino/pushups/CounterActivity.java
+++ app/src/main/java/com/cloudydino/pushups/CounterActivity.java
@@ -2,12 +2,24 @@ package com.cloudydino.pushups;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CounterActivity extends AppCompatActivity {
+
+    private static int numberOfPushups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_counter, menu);
+        return true;
     }
 }
