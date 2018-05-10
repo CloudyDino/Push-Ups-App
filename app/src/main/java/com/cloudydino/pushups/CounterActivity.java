1a96b3e6306e5d373fa05a3dc7dcbe69460a88de
Amaan Marfatia <amaanmarfatia@gmail.com>
2018-05-10 01:48:55 -0400

    Add multiple pushups

--- app/src/main/java/com/cloudydino/pushups/CounterActivity.java
+++ app/src/main/java/com/cloudydino/pushups/CounterActivity.java
@@ -1,13 +1,17 @@
 package com.cloudydino.pushups;
 
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CounterActivity extends AppCompatActivity {
 
@@ -24,12 +28,33 @@ public class CounterActivity extends AppCompatActivity {
         background = (ConstraintLayout) findViewById(R.id.constraintLayout_background);
         counter = (TextView) findViewById(R.id.textView_counter);
         incrementMessage = (TextView) findViewById(R.id.textView_incrementMessage);
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.floatingActionButton_add);
 
         numberOfPushups = 0;
         counter.setText(String.valueOf(numberOfPushups));
 
-        background.setOnClickListener(v -> {
-            changePushups(1);
+        background.setOnClickListener(v -> changePushups(1));
+
+        addButton.setOnClickListener(v -> {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            View dialogView = getLayoutInflater().inflate(R.layout.dialog_add_multiple, null);
+
+            dialogBuilder.setView(dialogView)
+                    .setTitle("Add how many push-ups?")
+                    .setNegativeButton("Cancel", (dI, i) -> {})
+                    .setPositiveButton("Add", (dI, i) -> {
+                        EditText editTextNumberOfPushups = (EditText) dialogView.findViewById(R.id.editText_numberOfPushups);
+                        String changeValue = editTextNumberOfPushups.getText().toString();
+                        try {
+                            changePushups(Integer.parseInt(changeValue));
+                        } catch (NumberFormatException e) {
+                            if (changeValue.length() > 0) {
+                                Toast.makeText(getApplicationContext(), "Number is too large", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+            dialogBuilder.create().show();
         });
     }
 
@@ -56,9 +81,14 @@ public class CounterActivity extends AppCompatActivity {
     }
 
     private void changePushups(int change) {
-        numberOfPushups += change;
-        if (numberOfPushups < 0) {
-            numberOfPushups = 0;
+        if (change > 0 && numberOfPushups + change < 0) {
+            numberOfPushups = Integer.MAX_VALUE;
+            Toast.makeText(getApplicationContext(), "Pushup counter can not go higher", Toast.LENGTH_SHORT).show();
+        } else {
+            numberOfPushups += change;
+            if (numberOfPushups < 0) {
+                numberOfPushups = 0;
+            }
         }
         updatePushups();
     }
